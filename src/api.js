3d0c7cf2755048547999f9b1629f39c0e76c24cb
Aarif <aarifsyed216@gmail.com>
2025-06-07 03:54:47 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5f58293ec3mshfe7bf7cb46c1954p1d4a27jsnfea656c6e170",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a7c4c9a958a277f92d467426e7b2fc51";
